2caf5915e92454eed1c6dc212d469f4139b8bed5
yoCasa <pr0gram3r0@gmail.com>
2025-10-06 00:59:04 +0200

    Terminado el Ahorcadov2.0

--- EjerciciosArrays02/src/Main.java
+++ EjerciciosArrays02/src/Main.java
@@ -9,6 +9,9 @@ public class Main {
     String palabraOculta="____________";
     char [] arrayLetras = new char[palabra.length()];
     char [] arrayAciertos = new char[palabra.length()];
+    int letrasUsadas = 0;
+    char [] arrayLetrasUsadas = new char[26];
+
     arrayAciertos = new char[]{'_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_'};
 
     char [] arrayHorca = new char[20];
@@ -18,35 +21,173 @@ public class Main {
     for(int i=0; i<palabra.length(); i++){
       arrayLetras[i]=palabra.charAt(i);
     }
-    System.out.println(new String(arrayLetras));
+
+    int intentos = 0;
 
     do {
-;
       System.out.println("Introduce una letra para descubrir la palabra oculta: ");
       char letra = teclado.next().charAt(0);
-      boolean coincidencia = false;
 
-      for (int i = 0; i < arrayLetras.length; i++) {
-        if (arrayLetras[i] == letra) {
-          arrayAciertos[i] = letra;
-          coincidencia=true;
+      boolean repetida = false;
+      for (int i = 0; i < letrasUsadas; i++) {
+        if (arrayLetrasUsadas[i] == letra) {
+          repetida = true;
+          break;
         }
       }
+      if (repetida) {
+        intentos++;
+        System.out.println("Ya has probado con la letra '" + letra + "'. ¡Pierdes un intento!");
+        if (intentos <= 11) {
+          dibujarHorca(intentos);
+        }
+      } else {
+        arrayLetrasUsadas[letrasUsadas] = letra;
+        letrasUsadas++;
 
-      if(coincidencia){
-        System.out.println(new String(arrayAciertos));
-      }else {
-        System.out.println("La letra '" + letra + "' no aparece en la palabra oculta.");
+        boolean coincidencia = false;
+        for (int i = 0; i < arrayLetras.length; i++) {
+          if (arrayLetras[i] == letra) {
+            arrayAciertos[i] = letra;
+            coincidencia = true;
+          }
+        }
 
+        if (coincidencia) {
+          System.out.println(new String(arrayAciertos));
+
+        } else {
+          intentos++;
+          System.out.println("La letra '" + letra + "' no aparece en la palabra oculta.");
+          if (intentos <= 11) {
+            dibujarHorca(intentos);
+          }
+
+        }
       }
+    }
+    while (!new String(arrayAciertos).equals(palabra) && intentos < 11) ;
 
-    }while(!new String(arrayAciertos).equals(palabra));
+    if (new String(arrayAciertos).equals(palabra)) {
+      System.out.println("¡Enhorabuena! La palabra oculta era \"" + palabra + "\"");
+    } else {
+      System.out.println("¡Has perdido! La palabra oculta era: \"" + palabra + "\"");
+    }
 
-    System.out.println("Enhorabuena! La palabra oculta era \"" + palabra + "\"");
 
   }
 
 
 
+
+
+
+  public static void dibujarHorca(int num){
+
+      switch(num) {
+        case 1:
+          System.out.println("Te quedan " + (11 - num) + " intentos.\n");
+          System.out.println("");
+          System.out.println("");
+          System.out.println("");
+          System.out.println("");
+          System.out.println("");
+          System.out.println("---");
+          break;
+        case 2:
+          System.out.println("Te quedan " + (11 - num) + " intentos.\n");
+          System.out.println("");
+          System.out.println("");
+          System.out.println("");
+          System.out.println(" |");
+          System.out.println(" |");
+          System.out.println("---");
+          break;
+        case 3:
+          System.out.println("Te quedan " + (11 - num) + " intentos.\n");
+          System.out.println("");
+          System.out.println(" |");
+          System.out.println(" |");
+          System.out.println(" |");
+          System.out.println(" |");
+          System.out.println("---");
+          break;
+        case 4:
+          System.out.println("Te quedan " + (11 - num) + " intentos.\n");
+          System.out.println("_____");
+          System.out.println(" |");
+          System.out.println(" |");
+          System.out.println(" |");
+          System.out.println(" |");
+          System.out.println("---");
+          break;
+        case 5:
+          System.out.println("Te quedan " + (11 - num) + " intentos.\n");
+          System.out.println("_____");
+          System.out.println(" |  |");
+          System.out.println(" |");
+          System.out.println(" |");
+          System.out.println(" |");
+          System.out.println("---");
+          break;
+        case 6:
+          System.out.println("Te quedan " + (11 - num) + " intentos.\n");
+          System.out.println("_____");
+          System.out.println(" |  |");
+          System.out.println(" |  O");
+          System.out.println(" |  ");
+          System.out.println(" |");
+          System.out.println("---");
+          break;
+        case 7:
+          System.out.println("Te quedan " + (11 - num) + " intentos.\n");
+          System.out.println("_____");
+          System.out.println(" |  |");
+          System.out.println(" |  O");
+          System.out.println(" |  |");
+          System.out.println(" |");
+          System.out.println("---");
+          break;
+        case 8:
+          System.out.println("Te quedan " + (11 - num) + " intentos.\n");
+          System.out.println("_____");
+          System.out.println(" |  |");
+          System.out.println(" |  O");
+          System.out.println(" | /|");
+          System.out.println(" |");
+          System.out.println("---");
+          break;
+        case 9:
+          System.out.println("Te quedan " + (11 - num) + " intentos.\n");
+          System.out.println("_____");
+          System.out.println(" |  |");
+          System.out.println(" |  O");
+          System.out.println(" | /|\\");
+          System.out.println(" |  ");
+          System.out.println("---");
+          break;
+        case 10:
+          System.out.println("Te quedan " + (11 - num) + " intentos.\n");
+          System.out.println("_____");
+          System.out.println(" |  |");
+          System.out.println(" |  O");
+          System.out.println(" | /|\\");
+          System.out.println(" | / ");
+          System.out.println("---");
+          break;
+        case 11:
+          System.out.println("Te quedan " + (11 - num) + " intentos.\n");
+          System.out.println("_____");
+          System.out.println(" |  |");
+          System.out.println(" |  O ");
+          System.out.println(" | /|\\            ¡ A H O R C A D O !");
+          System.out.println(" | / \\ ");
+          System.out.println("---");
+          break;
+        default:
+          break;
+      }
+
+  }
 
 }
