68cab73410e93c6968a58d5ccff2283469ef6329
Santiago Pérez Carral <a25santiagopc@teis.intranet>
2025-10-21 19:28:51 +0200

    Test02Resueltos

---
+++ FuncionesTest02/src/Main.java
@@ -0,0 +1,119 @@
+public class Main {
+
+
+    /*
+  Crear las siguientes funciones en el Main.
+  Recuerda que al estar dentro del main, las funciones deben de ser estáticas.
+  1. Crea una función que sea checkWordLength(String word) que devuelva la longitud de cada palabra que se le envíe como
+   parámetro.
+  2. Crea una función NumOfVowels(String word) que devuelva el número de vocales que contiene esa palabra. Para poder
+   realizar este ejercicio, deberás descomponer el String en un array de char[] con la función: toCharArray();
+  3. Crea una función llamada checkNumberOfPrimes(int[] numbers) la cual se le envíe como parámetro un array de números
+  y nos devuelva cuantos números primos hay. Crea otra función que sea isPrime(int i) que devuelva un boolean. Úsala en la
+    primera función.
+  4: Crea una función llamada isIdentityMatrix(int[][] matrix) que devuelva un booleano conforme si una matriz es
+   identidad o no. Recuerda que una matriz identidad es aquella que su diagonal de izquierda a derecha son
+    todo 1 y el resto son todo 0.
+
+	Ejemplos:
+
+	1	0	0
+	0	1	0
+	0	0	1
+
+
+	1	0	0	0
+	0	1	0	0
+	0	0	1	0
+	0	0	0	1
+     */
+
+
+
+  public static int checkWordLength(String word) {
+
+    int tamanho = 0;
+
+    tamanho = word.length();
+
+    return tamanho;
+  }
+
+
+  public static int NumOfVowels(String word) {
+
+    int cantVocales = 0;
+    word = word.toLowerCase();
+
+    char [] arrayWord = word.toCharArray();
+
+    char [] arrayVocales = {'a','e','i','o','u'};
+
+    for ( int i=0; i<arrayWord.length; i++){
+      for ( int j=0; j<arrayVocales.length; j++){
+        if (arrayWord[i] == arrayVocales[j]){
+          cantVocales++;
+        }
+      }
+    }
+
+    return cantVocales;
+  }
+
+  public static int checkNumberOfPrimes(int[] numbers) {
+
+    int cantOddNum = 0;
+
+    for ( int i=0; i<numbers.length; i++){
+      if (isPrime(numbers[i])){
+        cantOddNum++;
+      }
+    }
+    return cantOddNum;
+  }
+
+  public static boolean isPrime(int i){
+
+    int divisible = 0;
+    boolean esPrimo = true;
+    if ( i<=1) {
+      esPrimo = false;
+    }
+    for ( int j=1; j<=i; j++) {
+      if ( i % j == 0 ) {
+        divisible++;
+      }
+    }
+    if (divisible > 2){
+      esPrimo = false;
+    }
+    return esPrimo;
+  }
+
+  public static boolean isIdentityMatrix(int[][] matrix) {
+
+    boolean matrizIdentidad = true;
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        if (i == j) {
+          if (matrix[i][i] != 1) {
+            return false;
+          }
+        } else {
+          if (matrix[i][j] != 0) {
+            return false;
+          }
+        }
+      }
+    }
+    return matrizIdentidad;
+    //isIdentityMatrix
+  }
+
+
+
+
+
+  //Main
+}
--- ProbandoCode/src/Main.java
+++ ProbandoCode/src/Main.java
@@ -10,17 +10,37 @@ public class Main {
 
     //EJERCICIO_REPASO_04: Pedir solo un número. Determinar si es un número primo o no.
 
+    /*
     System.out.println("Introduzca un número para comprobar si es primo->");
     int numero = teclado.nextInt();
     esPrimo(numero);
+    */
+
+    int [] arrayAdo = {3, 4, 5, 2, 1};
+
+    imprimirArray(arrayAdo);
+    imprimirArray(algoritmoBurbuja(arrayAdo));
+
+
+
 
   }//main
 
+  public static void imprimirArray(int [] array){
+    System.out.print( "{ ");
+    for (int i=0; i<array.length; i++) {
+      System.out.print(array[i]);
+      if (i < array.length - 1) {
+        System.out.print(", ");
+      }
+    }
+    System.out.println(" }\n");
+    //imprimirArray
+  }
 
 
 
-
-  public static void esPrimo(int num){
+  /*public static void esPrimo(int num){
 
     int divisible = 0;
 
@@ -35,10 +55,28 @@ public class Main {
       System.out.println("El número " + num + " ES PRIMO.");
     }
 
+  }*/
+
+
+  public static int [] algoritmoBurbuja(int [] array){
+
+    //int [] arrayResultado = new int [array.length];
+
+    for ( int i=0; i< array.length-1; i++ ) {
+      for ( int j=0; j<array.length-i-1; j++){
+        if ( array[j] > array[j+1] ){
+          int maximo = array[j];
+          array[j] = array[j+1];
+          array[j+1] = maximo;
+        }
+      }
+
+    }
+    return array;
+
+
+    //main
   }
-
-
-
 
 
 //Main
